d2af79b3746795c9aaf6207dc8fe9f192b8e1229
Ashish-741 <ashishchahar731@gmail.com>
2025-08-28 11:18:40 +0530

    Create leetcode 49 gropu anagram.java

---
+++ leetcode 49 gropu anagram.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            String key = getKey(s);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+    
+    private String getKey(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int f : freq) {
+            sb.append(f).append('#'); // avoid ambiguity
+        }
+        return sb.toString();
+    }
+}
