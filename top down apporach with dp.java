59b09e2eb5d0210fb09e1cd6caf8610c3fe02e0e
Ashish-741 <ashishchahar731@gmail.com>
2025-09-03 11:42:46 +0530

    Create top down apporach with dp.java

---
+++ top down apporach with dp.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+//leetcode 198
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return TD(nums, 0, dp);
+    }
+
+    private int TD(int[] arr, int i, int[] dp) {
+        if (i >= arr.length) return 0;
+        if (dp[i] != -1) return dp[i];
+        int rob = arr[i] + TD(arr, i + 2, dp);
+        int not = TD(arr, i + 1, dp);
+        return dp[i] = Math.max(rob, not);
+    }
+}
